3c9ae6a4d757f4930525558a203d82c6f8a9a67a
Prince86 <pkbghl90@gmail.com>
2023-06-15 19:49:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import data from './data/data.json';
+
+const defaultValues = {
+
+}
+
 
 function App() {
+    const base = data.columns.slice(0,4);
+    console.log(base)
+  console.log(data.columns);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-800 min-h-screen">
+      <div className="px-16 py-12">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 shadow-lg">
+            {base.map((col) => {
+                return (
+                    <div className="flex justify-between px-4 py-2 items-center">
+                      <label className="text-slate-300 text-sm" htmlFor={col.label}>
+                        {col.label}:
+                      </label>
+                      <input defaultValue={col.default}  />
+                    </div>
+                  );
+            })}
+            <hr /><hr />
+          {data.columns.slice(4).map((col) => {
+            return (
+              <div className="flex justify-between px-4 py-2 items-center">
+                <label className="text-slate-300 text-sm" htmlFor={col.label}>
+                  {col.label}:
+                </label>
+                <input defaultValue={col.default}  />
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
